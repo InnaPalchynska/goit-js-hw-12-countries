3f492b55d082cbe5b54fd68b37a0dfb8a92b7051
Inna Palchynska <InnaPalchynska@gmail.com>
2021-05-30 19:30:17 +0300

    done

---
+++ src/index.js
@@ -0,0 +1,60 @@
+import getRefs from './get-refs.js';
+import API from './fetchCountries.js';
+
+import countryTpl from './country.hbs';
+
+import * as PNotify from '../node_modules/@pnotify/core/dist/PNotify.js';
+import * as PNotifyMobile from '../node_modules/@pnotify/mobile/dist/PNotifyMobile.js';
+import '@pnotify/core/dist/BrightTheme.css';
+
+PNotify.defaultModules.set(PNotifyMobile, {});
+var debounce = require('../node_modules/lodash.debounce');
+
+const refs = getRefs();
+refs.searchForm.addEventListener('input', debounce(onSearch, 500));
+
+function onSearch(event) {
+  const input = event.target;
+  const searchQuery = input.value;
+
+  API.fetchCountries(searchQuery).then(renderCountryCard).catch(onFetchError);
+}
+
+function renderCountryCard(country) {
+  refs.cardContainer.innerHTML = '';
+  if (country.length > 10) {
+    PNotify.error({
+      text: 'Too many matches found. Please enter a more specific query!',
+    });
+  }
+  if (country.length > 2 && country.length < 10) {
+    makeItems(country);
+  }
+
+  if (country.length === 1) {
+    makeCountryCard(country);
+  }
+}
+
+function onFetchError(error) {
+  PNotify.error({
+    title: 'Oh No!',
+    text: 'Something terrible happened.',
+  });
+}
+
+function makeItems(items) {
+  const itemsMarkup = items.map(item => {
+    const itemEl = document.createElement('p');
+    itemEl.textContent = item.name;
+
+    return itemEl;
+  });
+
+  refs.cardContainer.append(...itemsMarkup);
+}
+
+function makeCountryCard(country) {
+  const countryMarkup = countryTpl(country);
+  refs.cardContainer.insertAdjacentHTML('beforeend', countryMarkup);
+}
